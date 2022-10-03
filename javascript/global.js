00ccc7ad3175c1a4ade742386718ecb480765abd
Kurasad <pulavarthy.dhanush@gmail.com>
2022-10-02 22:21:21 -0500

    cache manager and animation toggle

--- javascript/global.js
+++ javascript/global.js
@@ -1,18 +1,17 @@
 const $ = (query, parent) => (parent || document).querySelector(query)
 const $$ = (query, parent) => (parent || document).querySelectorAll(query)
 const { style } = document.documentElement
-let mode = !window.matchMedia("(prefers-color-scheme: dark)").matches
+let mode = !window.matchMedia("(prefers-color-scheme: dark)").matches ? 'light' : 'dark'
 
 const handler = {
     set(data, prop, receiver) {
         Reflect.set(...arguments);
         build()
-        console.log(prop, receiver)
         return true
     },
 };
 
-let cache = new Proxy({ scroll: null, mode: mode ? 'light' : 'dark', notif: null, perspective: null, full: null }, handler)
+let cache = new Proxy({ scroll: null, mode: null, notif: null, perspective: null, full: null, animations: null }, handler)
 
 const ext = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 194.818 194.818"><g><path d="M185.818,2.161h-57.04c-4.971,0-9,4.029-9,9s4.029,9,9,9h35.312l-86.3,86.3c-3.515,3.515-3.515,9.213,0,12.728c1.758,1.757,4.061,2.636,6.364,2.636s4.606-0.879,6.364-2.636l86.3-86.3v35.313c0,4.971,4.029,9,9,9s9-4.029,9-9v-57.04C194.818,6.19,190.789,2.161,185.818,2.161z"/></g></svg>'
 $$("[external]").forEach(elem => {
@@ -29,24 +28,19 @@ window.onbeforeunload = _ => window.scrollTo(0, 0);
 
 window.onload = _ => {
     parse()
-    if (cache.mode !== (mode ? 'light' : 'dark')) {
-        style.setProperty('--background', cache.mode ? '#eeeeee' : '#111111')
-        style.setProperty('--color', cache.mode ? '#111111' : '#eeeeee')
-        style.setProperty('--elements', cache.mode ? '#dddddd' : '#222222')
-        style.setProperty('--hover', cache.mode ? '#0000001a' : '#ffffff1a')
-        style.setProperty('--shadow', cache.mode ? '#808080' : '#000000')
-        if ($('[view] div iframe')) $('[view] div iframe').src = cache.mode ? 'https://discordapp.com/widget?id=552142180098244619&theme=light' : 'https://discordapp.com/widget?id=552142180098244619&theme=dark'
-        mode = !mode
-    }
+    build()
+
+    console.log(cache)
+
+    if (cache.mode && cache.mode !== mode) toggle()
+
     if (cache.perspective || cache.full) {
         $('[mount]').style.transform = 'none';
         $('[mount]').style.height = '100vh';
         $('[home]').style.transform = 'none';
         $('[home]').style.height = '100vh';
         $('[view]').style.height = '100vh';
-        $('label span').style.marginLeft = '15px';
         $('[home]').style.width = '100%';
-        $('label span').style.marginLeft = '15px';
         $('[mount]').style.transitionDuration = '0';
         $('[home]').style.transitionDuration = '0';
         style.setProperty('--perspective', 'none')
@@ -56,6 +50,8 @@ window.onload = _ => {
         style.setProperty('--flow', 'block')
         $('nav').style.display = 'none';
     }
+
+    if (cache.animations) $('[animations]').checked = false
 
     $('[top]').onclick = _ => $('[view]').scrollTo({ top: 0, behavior: 'smooth' });
 
@@ -80,10 +76,7 @@ window.onload = _ => {
             setTimeout(_ => {
                 $('[preloader] svg').style.fill = 'var(--background)';
                 $('[preloader]').classList.add('hide')
-                setTimeout(_ => {
-                    $('[preloader]').remove()
-                    // if (cache.scroll && $(`[${cache.scroll}]`)) $(`[${cache.scroll}]`).scrollIntoView({ behavior: 'smooth' })
-                }, 400)
+                setTimeout(_ => $('[preloader]').remove(), 400)
             }, 250)
         }, 200)
     }, 400)
@@ -92,30 +85,33 @@ window.onload = _ => {
 
 
 function toggle(force) {
-    mode = force ? force === 'light' : mode
-    style.setProperty('--background', !mode ? '#eeeeee' : '#111111')
-    style.setProperty('--color', !mode ? '#111111' : '#eeeeee')
-    style.setProperty('--elements', !mode ? '#dddddd' : '#222222')
-    style.setProperty('--hover', !mode ? '#0000001a' : '#ffffff1a')
-    style.setProperty('--shadow', !mode ? '#808080' : '#000000')
-    if ($('[view] div iframe')) $('[view] div iframe').src = !mode ? 'https://discordapp.com/widget?id=552142180098244619&theme=light' : 'https://discordapp.com/widget?id=552142180098244619&theme=dark'
-    mode = !mode
-    cache.mode = mode ? 'light' : 'dark'
+    mode = mode === 'dark' ? 'light' : 'dark'
+    style.setProperty('--background', mode === 'light' ? '#eeeeee' : '#111111')
+    style.setProperty('--color', mode === 'light' ? '#111111' : '#eeeeee')
+    style.setProperty('--elements', mode === 'light' ? '#dddddd' : '#222222')
+    style.setProperty('--hover', mode === 'light' ? '#0000001a' : '#ffffff1a')
+    style.setProperty('--shadow', mode === 'light' ? '#808080' : '#000000')
+    if ($('[view] div iframe')) $('[view] div iframe').src = mode === 'light' ? 'https://discordapp.com/widget?id=552142180098244619&theme=light' : 'https://discordapp.com/widget?id=552142180098244619&theme=dark'
+    cache.mode = mode
+    build()
+}
+
+function animations() {
+    cache.animations = !cache.animations
     build()
 }
 
 function perspective() {
     if (window.innerWidth < 1200) return
     const nav = $('[mount]').style.transform === 'none'
-    $('[mount]').style.transitionDuration = '200ms';
-    $('[home]').style.transitionDuration = '200ms';
+    $('[mount]').style.transitionDuration = !parse().animations ? '200ms' : '0';
+    $('[home]').style.transitionDuration = !parse().animations ? '200ms' : '0';
     $('[mount]').style.transform = nav ? 'perspective(100vw) rotateY(30deg)' : 'none';
     $('[mount]').style.height = nav ? '70vh' : '100vh';
     $('[home]').style.transform = nav ? 'perspective(100vw) rotateY(-10deg)' : 'none';
     $('[home]').style.height = nav ? '90vh' : '100vh';
     $('[view]').style.height = nav ? '90vh' : '100vh';
     $('[home]').style.width = nav ? '95%' : '100%';
-    $('label span').style.marginLeft = nav ? '5px' : '15px';
     $('[mount]').style.transitionDuration = '0';
     $('[home]').style.transitionDuration = '0';
     style.setProperty('--perspective', nav ? 'auto' : 'none');
@@ -129,13 +125,13 @@ const random = (sections = 5, phrase = 5, join = '-') => {
     return [...Array(sections)].map(_ => [...Array(phrase)].map(pick).join('')).join(join);
 };
 
-function parse(href, flip) {
-    const args = href ? href.split('#')[1].split('&') : window.location.hash.slice(1).split('&') || []
+function parse() {
+    const args = localStorage.getItem('marksbot-cache').split('&') || []
     if (!args.length) return
     const obj = []
     args.map(e => e.split('=')).map(([key, value]) => obj[key] = value)
     const result = { ...cache, ...obj }
-    cache = href ? cache : result
+    cache = result
     return result
 }
 
@@ -143,15 +139,5 @@ function build() {
     const result = []
     if (cache.mode === (window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light')) delete cache.mode
     for (const key in cache) if (cache[key]) result.push(`${key}=${cache[key]}`)
-    $$('[tabs] a').forEach(elem => {
-        if (!elem.hasAttribute('external')) elem.href = `${elem.href.split('#')[0]}#${result.join('&')}`
-    })
-    window.location.hash = result.length ? result.join('&') : ''
-}
-
-function scrollFrom(element) {
-    if (element.href) {
-        $(`[${parse(element.href).scroll}]`).scrollIntoView({ behavior: 'smooth' })
-        cache.scroll = parse(element.href).scroll
-    }
+    localStorage.setItem('marksbot-cache', result.length ? result.join('&') : '')
 }
